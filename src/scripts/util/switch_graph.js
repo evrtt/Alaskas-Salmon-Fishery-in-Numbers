5b3aeb6a4652b7dc6a0a8bd2715a3ffa24e2d0bb
heavyevy <everettsmith95@gmail.com>
2021-06-25 20:15:24 -0600

    Complete switchGraph functions

--- src/scripts/util/switch_graph.js
+++ src/scripts/util/switch_graph.js
@@ -1,46 +1,61 @@
 import { drawTransitionWave, lowerWave } from './wave.js'
+import { genBubbles } from '../transitions/intro_to_spatial.js'
+import { clearCharts } from './bubble_chart.js'
 
 export const switchGraphRaise = (amt, toGraph) => {
   
   drawTransitionWave();
 
-    const waveTop = document.getElementsByClassName('wave-top')[0]
-    const waveBottom = document.getElementsByClassName('wave-bottom')[0]
+  const waveTop = document.getElementsByClassName('wave-top')[0]
+  const waveBottom = document.getElementsByClassName('wave-bottom')[0]
 
-    if (amt >= window.innerHeight - 1) {
-      window.requestAnimationFrame(() => switchGraphLower(amt, direction, prevButton, nextButton))
+  if (amt >= window.innerHeight - 1) {
+    if(toGraph === 'byYear') {
+      genBubbles('1979', 'set')
+      switchBoundingBoxes(toGraph)
     } else {
-      amt += Math.sqrt(window.innerHeight - amt)
-      waveTop.style.bottom = `${amt}px`
-      waveBottom.style.height = `${amt + 1}px`
-      window.requestAnimationFrame(() => switchGraphRaise(amt, toGraph))
+      clearCharts()
+      switchBoundingBoxes(toGraph)
     }
+    window.requestAnimationFrame(() => switchGraphLower(amt))
+  } else {
+    amt += Math.sqrt(window.innerHeight - amt)
+    waveTop.style.bottom = `${amt}px`
+    waveBottom.style.height = `${amt + 1}px`
+    window.requestAnimationFrame(() => switchGraphRaise(amt, toGraph))
+  }
 }
 
-export const switchGraphLower = () => {
+export const switchGraphLower = (amt) => {
 
   const waveTop = document.querySelector('.wave-top')
   const waveBottom = document.querySelector('.wave-bottom')
 
 
   if (amt > window.innerHeight) {
-      amt -= Math.sqrt(100)
-      waveTop.style.bottom = `${amt}px`
-      waveBottom.style.height = `${amt}px`
-      window.requestAnimationFrame(() => lowerWave(amt, direction, prevButton, nextButton))
+    amt -= Math.sqrt(100)
+    waveTop.style.bottom = `${amt}px`
+    waveBottom.style.height = `${amt}px`
+    window.requestAnimationFrame(() => lowerWave(amt))
   } else if (amt > 0) {
-      amt -= Math.sqrt(100 + (window.innerHeight - amt))
-      waveTop.style.bottom = `${amt}px`
-      waveBottom.style.height = `${amt}px`
-      window.requestAnimationFrame(() => lowerWave(amt, direction, prevButton, nextButton))
+    amt -= Math.sqrt(100 + (window.innerHeight - amt))
+    waveTop.style.bottom = `${amt}px`
+    waveBottom.style.height = `${amt}px`
+    window.requestAnimationFrame(() => lowerWave(amt))
   } else if (amt < 0 && amt > -420) {
-      amt -= Math.sqrt(100 + (window.innerHeight - amt))
-      waveTop.style.bottom = `${amt}px`
-      waveBottom.style.height = 0
-      window.requestAnimationFrame(() => lowerWave(amt, direction, prevButton, nextButton))
+    amt -= Math.sqrt(100 + (window.innerHeight - amt))
+    waveTop.style.bottom = `${amt}px`
+    waveBottom.style.height = 0
+    window.requestAnimationFrame(() => lowerWave(amt))
   } else if (amt < -420) {
-      waveTop.remove()
-      const current = document.querySelector('.current')
-    }
-  
+    waveTop.remove()
+  }
+}
+
+switchBoundingBoxes = (toGraph) => {
+  if (toGraph === 'byYear') {
+
+  } else {
+
+  }
 }
--- src/scripts/util/wave.js
+++ src/scripts/util/wave.js
@@ -127,131 +127,6 @@ export const drawTransitionWave = () => {
   }
 }
 
-// export const drawStaticWave = (color, element) => {
-
-//   let selector = color[0] === '#' ? color.slice(1) : color
-
-//   let iterator = 120
-//   let canvas = document.querySelector(`.static-wave-${selector}`)
-//   if (canvas) {
-//     null
-//   } else {
-//     canvas = document.createElement('canvas');
-//     canvas.classList.add(`static-wave-${selector}`);
-//     element.appendChild(canvas)
-//   }
-
-//   const context = canvas.getContext('2d');
-//   const radius = 1000
-
-//   window.requestAnimationFrame(() => drawFrame(iterator))
-
-//   const drawFrame = (iterator) => {
-    
-
-//     const width = (Math.floor(window.innerWidth / 1000) + 2) * 1000
-//     canvas.width = context.width = window.innerWidth
-//     const height = canvas.height = context.height = 420
-//     const numLoops = (width) / 1000 + 2
-//     let changeSmallX;
-//     let moveCenter;
-
-    
-
-//     const mod60 = iterator % 60
-//     const mod30 = iterator % 30
-//     if (iterator === 0) {
-//       changeSmallX = radius
-//       moveCenter = 0
-//     } else if (iterator > 0 && iterator < 30) {
-//       changeSmallX = 2 * radius * (Math.sin(((Math.PI * (60 - (mod30 * 2))) / 180) / 2))
-//       moveCenter = ((radius / 2) - (changeSmallX / 2))
-      
-//     } else if (iterator === 30) {
-//       changeSmallX = 0
-//       moveCenter = radius / 2
-      
-//     } else if (iterator > 30 && iterator < 60) {
-//       changeSmallX = 2 * radius * (Math.sin(((Math.PI * mod30 * 2) / 180) / 2))
-//       moveCenter = radius - ((radius / 2) - (changeSmallX / 2))
-      
-//     } else if (iterator === 60) {
-//       changeSmallX = radius
-//       moveCenter = radius
-      
-//     } else if (iterator > 60 && iterator < 90) {
-//       changeSmallX = 2 * radius * (Math.sin(((Math.PI * (60 - (mod30 * 2))) / 180) / 2))
-//       moveCenter = ((radius / 2) - (changeSmallX / 2)) + radius
-      
-//     } else if (iterator === 90) {
-//       changeSmallX = 0
-//       moveCenter = radius * 1.5
-      
-//     } else if (iterator > 90 && iterator < 120) {
-//       changeSmallX = 2 * radius * (Math.sin(((Math.PI * mod30 * 2) / 180) / 2))
-//       moveCenter = radius * 2 - ((radius / 2) - (changeSmallX / 2))
-//     }
-
-//     const changeY = Math.sqrt((radius ** 2) - ((radius / 2) ** 2)) * 2
-
-//     context.clearRect(0, 0, width, height)
-//     for (let i = 0; i < numLoops; i++) {
-
-//       if (i === 0) {
-
-//         if (iterator > 59) {
-
-//           context.beginPath()
-//           context.arc((radius * ((2 * i) + 1)) + (radius / 2) - moveCenter, changeY - 580, radius, Math.PI * 240 / 180, Math.PI * 300 / 180, false)
-//           context.lineTo(radius * 2 - (moveCenter - 1), (changeY / 2) - 580)
-//           context.lineTo(radius * 2 - (moveCenter - 1), height + 1)
-//           context.lineTo(0, height + 1)
-//           context.fillStyle = color
-//           context.fill()
-
-//         } else {
-
-//           context.beginPath()
-//           context.arc((radius / 2) - moveCenter, -580, radius, (Math.PI * 60) / 180, (Math.PI * (120 - mod60)) / 180, false)
-//           context.lineTo(0, height + 1)
-//           context.lineTo(radius * 2 - (moveCenter - 1), height + 1)
-//           context.lineTo(radius * 2 - (moveCenter - 1), (changeY / 2) - 580)
-//           context.moveTo((radius * ((2 * i) + 1)) + (radius / 2) - moveCenter, (changeY / 2) - 580)
-//           context.arc((radius * ((2 * i) + 1)) + (radius / 2) - moveCenter, changeY - 580, radius, Math.PI * 240 / 180, Math.PI * 300 / 180, false)
-//           context.fillStyle = color
-//           context.fill()
-//         }
-
-//       } else if (i === numLoops - 1) {
-
-//         context.beginPath()
-//         context.arc((radius / 2) + (radius * i * 2) - moveCenter, -580, radius, (Math.PI * 60) / 180, (Math.PI * 120) / 180, false)
-//         context.lineTo(radius * 2 * i - moveCenter, height + 1)
-//         context.lineTo(radius * (2 + (i * 2)) - moveCenter, height + 1)
-//         context.lineTo(radius * (2 + (i * 2)) - moveCenter, (changeY / 2) - 580)
-//         context.moveTo((radius * ((2 * i) + 1)) + (radius / 2) - moveCenter, (changeY / 2) - 580)
-//         context.arc((radius * ((2 * i) + 1)) + (radius / 2) - moveCenter, changeY - 580, radius, Math.PI * 240 / 180, Math.PI * 300 / 180, false)
-//         context.fillStyle = color
-//         context.fill()
-//       } else {
-
-//         context.beginPath()
-//         context.arc((radius / 2) + (radius * i * 2) - moveCenter, -580, radius, (Math.PI * 60) / 180, (Math.PI * 120) / 180, false)
-//         context.lineTo(0 + radius * 2 * i - moveCenter, height + 1)
-//         context.lineTo(radius * (2 + (i * 2)) - moveCenter, height + 1)
-//         context.lineTo(radius * (2 + (i * 2)) - moveCenter, (changeY / 2) - 580)
-//         context.moveTo((radius * ((2 * i) + 1)) + (radius / 2) - moveCenter, (changeY / 2) - 580)
-//         context.arc((radius * ((2 * i) + 1)) + (radius / 2) - moveCenter, changeY - 580, radius, Math.PI * 240 / 180, Math.PI * 300 / 180, false)
-//         context.fillStyle = color
-//         context.fill()
-//       }
-//     }
-
-//     iterator = iterator === 0 ? 119.5 : iterator - 0.5
-//     window.requestAnimationFrame(() => drawFrame(iterator))
-//   }
-// }
-
 export const raiseWave = (amt, direction, toView, fromView) => {
   
   const waveTop = document.getElementsByClassName('wave-top')[0]
@@ -294,13 +169,8 @@ export const raiseWave = (amt, direction, toView, fromView) => {
         document.getElementById(buttons[toView.id.split('-').join('')][0]),
         buttons[toView.id.split('-').join('')][1]
        ]
-      // prevButton = [
-      //   document.getElementById(buttons[fromView.id.split('-').join('')][0]),
-      //   buttons[fromView.id.split('-').join('')][1]
-      // ]
       toggleView(fromView)
       toggleView(toView)
-      // document.querySelector('.scroll-down').addEventListener('click', () => toIntro(current))
     }
     window.requestAnimationFrame(() => lowerWave(amt, direction, prevButton, nextButton) )
   } else {
@@ -392,12 +262,6 @@ export const lowerWave = (amt, direction, prevButton, nextButton) => {
       window.requestAnimationFrame(() => lowerWave(amt, direction, prevButton, nextButton))
     } else if (amt - window.innerHeight < 0 && amt > 10) {
       amt -= Math.sqrt((window.innerHeight - (window.innerHeight - amt)) / 4)
-      // const vertOffset = 85 + (window.innerHeight - amt) / 20
-      // if (vertOffset >= 105) {
-      //   prevButton[0].style.display = "none"
-      // } else {
-      //   prevButton[0].style.top = (`${vertOffset}%`)
-      // }
       nextButton[0].style.top = '85%'
       nextButton[0].style.display = 'block'
       nextButton[0].style.left = `${50 + amt / 10}%`
@@ -407,29 +271,11 @@ export const lowerWave = (amt, direction, prevButton, nextButton) => {
     } else if (amt <= 10) {
       waveTop.style.bottom = '0px'
       waveBottom.style.height = '0px'
-      // prevButton[0].classList.toggle('current-button')
-      // prevButton[0].classList.toggle('next-button')
       nextButton[0].classList.toggle('current-button')
       nextButton[0].classList.toggle('next-button')
       const current = document.querySelector('.current')
       console.log(current)
       nextButton[0].addEventListener('click', () => nextButton[1](current))
     }
-  }
-}
-
-const slowDown = (height) => {
-
-  const waveTop = document.querySelector('.wave-top')
-  const waveBottom = document.querySelector('.wave-bottom')
-
-  if (height > 0) {
-    height /= 3
-    waveBottom.style.height = `${height}px`
-    waveTop.style.bottom = `${height}px`
-    window.requestAnimationFrame(() => slowDown(height))
-  } else {
-    waveBottom.style.height = '0px'
-    waveTop.style.bottom = `${0}px`
   }
 }
